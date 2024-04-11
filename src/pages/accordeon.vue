3b777dfbff6844dfb1e46734495e1f026cdc90b1
chloe-vincent03 <chloe.vincent03@edu.univ-fcomte.fr>
2024-04-11 16:20:18 +0200

    mise en place des section et des texte dans la partie accordeon

--- src/pages/accordeon.vue
+++ src/pages/accordeon.vue
@@ -2,11 +2,21 @@
 //
 import { ref } from 'vue';
 
-const maVar = ref(false)
-console.log('maVar:', maVar.value)
 
 </script>
 
 <template>
   <h1>Accordeon</h1>
+  <section>
+    <button class="text-xl">bouton 1</button>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, placeat fuga accusamus eveniet enim, aliquid in eos, ipsa blanditiis rerum explicabo. Aliquid aut eaque delectus veniam eius necessitatibus magni nobis.</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 2</button>
+    <p>Rerum eveniet iusto rem mollitia alias saepe corrupti sapiente similique officia voluptates delectus dicta amet voluptatum, ea dolor. Quibusdam molestias velit quas repudiandae expedita adipisci cumque quam totam cum maiores.</p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 3</button>
+    <p>Voluptatibus tempore nisi animi vel distinctio, quam, laudantium nostrum laboriosam, maiores temporibus corporis omnis? Commodi inventore vel voluptate, recusandae ipsam quos minus cupiditate ut atque, nisi, fuga quia tenetur reiciendis!</p>
+  </section>
 </template>
