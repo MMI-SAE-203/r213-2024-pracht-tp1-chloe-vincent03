a4a8fe8e506170e31aef3270fb34fd03bbbd935e
chloe-vincent03 <chloe.vincent03@edu.univ-fcomte.fr>
2024-04-11 15:36:13 +0200

    suivis du prof en td_ ne pas prendre en compte pour le TP

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 //
+import { ref } from 'vue';
+
+const maVar = ref(false)
+console.log('maVar:', maVar.value)
+
 </script>
 
 <template>
-  <h1 class="text-2xl">Bonjour monde !</h1>
+  <button @pointerdown="maVar = true">Montrer</button>
+  <button @pointerdown="maVar = false">Cacher</button>
+  <h1 v-show="maVar" class="text-2xl">Bonjour toi !</h1>
 </template>
